13bf497834c1c642ecf2e9e316c0615a0dd58157
bharatdwithi <87705473+Dwithi666@users.noreply.github.com>
2021-08-27 11:49:17 +0530

    Add files via upload
    committing code files

---
+++ SRCTest.java
@@ -0,0 +1,13 @@
+package com.create.Springboot.Sms.App1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSmsApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringbootSmsApp1Application.java
@@ -0,0 +1,13 @@
+package com.create.Springboot.Sms.App1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSmsApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSmsApp1Application.class, args);
+	}
+
+}
